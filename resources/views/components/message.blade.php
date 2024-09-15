f8b3d57d4f57564fc07cdca432938727d0c34a32
Ax3lrod <aryasatyagigachad9@gmail.com>
2024-09-15 23:15:31 +0700

    fixed message section responsiveness

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,4 +1,4 @@
-<div class="lg:h-[500px] h-[600px] flex text-white flex-col">
+<div class="h-[600px] flex text-white flex-col">
     <div class="flex w-full">
         <div class="flex justify-center flex-col w-1/2 lg:px-10 px-5">
             <div class="font-bold text-gradient lg:text-3xl text-2xl">Get In Touch</div>
@@ -8,18 +8,18 @@
         </div>
         <div class="flex justify-center items-center w-1/2">
             <img src="/antasena-logo-white.png" width="200" height="200"
-                class="lg:w-[200px] lg:h-[200px] w-[100px] h-[50px]">
+                class="lg:w-[200px] lg:h-[100px] w-[150px] h-[100px]">
         </div>
     </div>
     <div class="flex w-full mt-5 lg:pl-10 pl-5 flex-col">
-        <div class="w-full pr-10">
+        <div class="w-full lg:pr-10 pr-5">
             <p class="pl-1">Email</p>
             <input
                 class="bg-white w-full h-[38px] rounded-xl mt-2 border-[#5D2CE8] border-[3px] text-black flex items-center pl-3"
                 placeholder="Enter your email.">
             </input>
         </div>
-        <div class="w-full pr-10 mt-2">
+        <div class="w-full lg:pr-10 mt-2 pr-5">
             <p class="pl-1">Feel free to ask us anything. We’ll be happy to answer.</p>
             <input
                 class="bg-white w-full h-[200px] rounded-xl mt-2 border-[#5D2CE8] border-[3px] text-black flex items-start pl-3 pt-2"
